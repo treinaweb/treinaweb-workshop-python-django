baccbfa9e82a9e412207d9efb8896cff3cbaa5f8
Cleyson Lima <cleysonph@gmail.com>
2024-09-23 19:58:13 -0300

    04:09 - Criando a página de registro

--- setup/settings.py
+++ setup/settings.py
@@ -44,6 +44,7 @@ THIRD_PARTY_APPS = []
 
 LOCAL_APPS = [
     "core.apps.CoreConfig",
+    "accounts.apps.AccountsConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- setup/urls.py
+++ setup/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls", namespace="core")),
+    path("accounts/", include("accounts.urls", namespace="accounts")),
 ]
