4339ae8295e296b54781f87dbf504cc3de57cd13
Cleyson Lima <cleysonph@gmail.com>
2024-09-23 17:03:09 -0300

    03:07 - Criando a página sobre

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import home
+from .views import home, about
 
 
 app_name = "core"
 urlpatterns = [
     path("", home, name="home"),
+    path("about/", about, name="about"),
 ]
