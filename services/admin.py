f8b3845f15b731666f3fbeb67fd4568e273da6df
Cleyson Lima <cleysonph@gmail.com>
2024-09-23 22:21:34 -0300

    05:14 - Ajustando as permissões para o modelo Service no admin

--- services/admin.py
+++ services/admin.py
@@ -2,4 +2,29 @@ from django.contrib import admin
 
 from .models import Service
 
-admin.site.register(Service)
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "user")
+    list_filter = ("user",)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
+
+    def save_model(self, request, obj, form, change):
+        if not request.user.is_superuser:
+            obj.user = request.user
+        return super().save_model(request, obj, form, change)
+
+    def get_exclude(self, request, obj):
+        if not request.user.is_superuser and not obj:
+            return ["user"]
+        return []
+
+    def get_readonly_fields(self, request, obj):
+        if obj and not request.user.is_superuser:
+            return ["user"]
+        return []
