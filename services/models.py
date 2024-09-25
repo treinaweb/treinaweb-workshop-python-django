6eb46b592a56a008103e31e62456a0cc3c7436fd
Cleyson Lima <cleysonph@gmail.com>
2024-09-24 22:23:14 -0300

    05:19 - Criando o modelo ServiceOrder

--- services/models.py
+++ services/models.py
@@ -1,5 +1,7 @@
+from typing import Iterable
 from django.db import models
 from django.contrib.auth.models import User
+from uuid import uuid4
 
 
 def service_upload_to(instance, filename):
@@ -19,3 +21,37 @@ class Service(models.Model):
     class Meta:
         verbose_name = "Serviço"
         verbose_name_plural = "Serviços"
+
+
+class ServiceOrder(models.Model):
+    class Status(models.TextChoices):
+        OPEN = "OPEN", "Aberta"
+        CANCELED = "CANCELED", "Cancelada"
+        DONE = "DONE", "Finalizada"
+        FINISHED = "FINISHED", "Concluída"
+
+    code = models.UUIDField(default=uuid4, editable=False)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+    extra_info = models.TextField()
+    status = models.CharField(
+        max_length=10, choices=Status.choices, default=Status.OPEN
+    )
+
+    def __str__(self):
+        return str(self.code)
+
+    class Meta:
+        verbose_name = "Oderm de Serviço"
+        verbose_name_plural = "Ordens de Serviço"
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            self.service.user.email_user(
+                subject="Nova ordem de serviço",
+                message=f"Você tem uma nova ordem de serviço para o serviço {self.service.name}",
+            )
+        return super().save(args, kwargs)
