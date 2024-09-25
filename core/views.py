dbec3c8893b1110b57258c7765343af8c7304c9b
Cleyson Lima <cleysonph@gmail.com>
2024-09-24 21:29:53 -0300

    05:17 - Implementando os filtros de consulta na listagem de serviços

--- core/views.py
+++ core/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+from services.models import Service
 from .forms import ContactForm
 
 
 def home(request):
-    return render(request, "core/home.html")
+    cities = (
+        Service.objects.filter(user__profile__isnull=False)
+        .values("user__profile__city")
+        .distinct()
+    )
+    cities = [city["user__profile__city"] for city in cities]
+    return render(request, "core/home.html", {"cities": cities})
 
 
 def about(request):
--- services/views.py
+++ services/views.py
@@ -6,9 +6,23 @@ from .models import Service
 
 
 def list(request):
-    services = Service.objects.all()
+    q = request.GET.get("q", "")
+    city = request.GET.get("city")
+
+    services = Service.objects.filter(name__icontains=q)
+    if city:
+        services = services.filter(user__profile__city=city)
     paginator = Paginator(services, settings.PAGE_SIZE)
 
     page_number = request.GET.get("page", 1)
     page_obj = paginator.get_page(page_number)
-    return render(request, "services/list.html", {"page_obj": page_obj})
+
+    cities = (
+        Service.objects.filter(user__profile__isnull=False)
+        .values("user__profile__city")
+        .distinct()
+    )
+    cities = [city["user__profile__city"] for city in cities]
+    return render(
+        request, "services/list.html", {"page_obj": page_obj, "cities": cities}
+    )
